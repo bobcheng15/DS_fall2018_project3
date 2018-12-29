e79c2bbf4b712949d7ae8e322da35c932cf06edf
Robert Cheng <bobcheng15@gmail.com>
2018-12-29 22:42:06 +0800

    restructured and first implementation of code

--- student.h
+++ student.h
@@ -1,9 +1,86 @@
-
+enum Color{
+        White, // Initital 0
+        Blue, // Player One
+        Red, // Player Two
+        Black //Explosion
+    };
 class Student{
     public:
         void makeMove(int Record[5][6], int Max[5][6], Color color[5][6], Color inputColor){
-            // Your Code
-
+            x = 1;
+            y = 1;
+            for (int i = 0; i < 5; i ++){
+                for (int j = 0; j < 6; j ++){
+                    if (Record[i][j] == Max[5][6] - 1 && should_attack(Record, Max, color, inputColor, i, j)){
+                        x = i;
+                        y = j;
+                        return;
+                    }
+                    else if (is_corner(i, j) && !is_corner(x, y)){
+                        if (color[i][j] == White){
+                            x = i;
+                            y = j;
+                        }
+                    }
+                    else if (is_edge(i, j){
+                        if (!is_corner(x, y) && !is_edge(x, y)) {
+                            x = i;
+                            y = j;
+                        }
+                        else if (is_edge(x, y) && Max[x][y] - Record[x][y] < Max[i][j] - Record[i][j]){
+                            x = i;
+                            y = j;
+                        }
+                    }
+                    else if (!is_corner(x, y) && !is_edge(x, y) && !is_corner(i, j) && !is_edge(i, j)){
+                        if (color[i][j] == White){
+                            x = i;
+                            y = j;
+                        }
+                    }
+                }
+            }
+        }
+        bool should_attack(int Record[5][6], int Max[5][6], Color color[5][6], Color inputColor, const int & i, const int & j){
+            if (is_valid(i - 1, j)){
+                if (color[i - 1][j] != inputColor && Record[i - 1][j] == Max[i - 1][j] - 1){
+                    return true;
+                }
+            }
+            if (is_valid(i + 1, j)){
+                if (color[i + 1][j] != inputColor && Record[i + 1][j] == Max[i + 1][j] - 1){
+                    return true;
+                }
+            }
+            if (is_valid(i, j + 1)){
+                if (color[i][j + 1] != inputColor && Record[i][j + 1] == Max[i][j + 1] - 1){
+                    return true;
+                }
+            }
+            if (is_valid(i, j - 1)){
+                if (color[i][j - 1] != inputColor && Record[i][j - 1] == Max[i][j - 1] - 1){
+                    return true;
+                }
+            }
+            return false;
+        }
+        bool is_corner (const int & i, const int & j){
+            if (i == 0 && j == 0) return true;
+            if (i == 4 && j == 0) return true;
+            if (i == 0 && j == 5) return true;
+            if (i == 4 && j == 5) return true;
+            return false;
+        }
+        bool is_edge (const int & i, const int & j){
+            if (i == 0) return true;
+            if (j == 0) return true;
+            if (i == 4) return true;
+            if (j == 5) return true;
+            return false;
+        }
+        bool is_valid(const int & i, const int & j){
+            if (i >= 0 && i < 5 && j >= 0; j <= 6) return true;
+            else return false;
         }
         int getX(){
             // Your Code
