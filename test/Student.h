0b677eaa00e08f8bc60dfa301145af9219f36d50
Robert Cheng <bobcheng15@gmail.com>
2018-12-30 15:13:13 +0800

    added testcase to test is_playable function

--- test/Student.h
+++ test/Student.h
@@ -1,12 +1,22 @@
 #ifndef STUDENT_H_INCLUDED
 #define STUDENT_H_INCLUDED
+enum Color{
+        White, // Initital 0
+        Blue, // Player One
+        Red, // Player Two
+        Black //Explosion
+    };
+
 class Student{
 public:
     Student(){
         x = 5;
         y = 6;
     }
-    
+    bool is_playable(Color color[5][6], const int & i, const int & j, Color inputColor){
+        if (color[i][j] == White || color[i][j] == inputColor) return true;
+        else return false;
+    }
     int x;
     int y;
 };
--- test/utStudent.h
+++ test/utStudent.h
@@ -7,6 +7,35 @@ TEST(Student, testStudentConstructor){
     EXPECT_EQ(stud.y, 6);
 }
 
+TEST(Student, testStudentIsPlayable){
+    Color color[5][6];
+    for (int i = 0; i < 5; i ++){
+        for (int j = 0; j < 6; j ++){
+            color[i][j] = White;
+        }
+    }
+    Student stud = Student();
+    int i = 0;
+    int j = 0;
+    bool playable = stud.is_playable(color, i, j, Blue);
+    EXPECT_TRUE(playable);
+    i = 1;
+    j = 0;
+    color[i][j] = Blue;
+    playable = stud.is_playable(color, i, j, Blue);
+    EXPECT_TRUE(playable);
+    i = 2;
+    j = 0;
+    color[i][j] = Black;
+    playable = stud.is_playable(color, i, j, Blue);
+    EXPECT_FALSE(playable);
+    i = 3;
+    j = 0;
+    color[i][j] = Red;
+    playable = stud.is_playable(color, i, j, Blue);
+    EXPECT_FALSE(playable);
+}
+
 
 
 
