88549a04da06ee203904d33fbb063590f480c4a4
Robert Cheng <bobcheng15@gmail.com>
2018-12-30 13:56:09 +0800

    added testcase to test constructor of class Student

--- test/Student.h
+++ test/Student.h
@@ -1,6 +1,15 @@
 #ifndef STUDENT_H_INCLUDED
 #define STUDENT_H_INCLUDED
-
+class Student{
+public:
+    Student(){
+        x = 5;
+        y = 6;
+    }
+    
+    int x;
+    int y;
+};
 
 
 
--- test/utStudent.h
+++ test/utStudent.h
@@ -1,6 +1,11 @@
 #ifndef UTSTUDENT_H_INCLUDED
 #define UTSTUDENT_H_INCLUDED
-
+#include "Student.h"
+TEST(Student, testStudentConstructor){
+    Student stud = Student();
+    EXPECT_EQ(stud.x, 5);
+    EXPECT_EQ(stud.y, 6);
+}
 
 
 
