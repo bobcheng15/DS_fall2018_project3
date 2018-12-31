7ca21753082c26ca38795bd8c76491e974bb40f4
Robert Cheng <bobcheng15@gmail.com>
2018-12-31 12:09:21 +0800

    second submission

--- student.cpp
+++ student.cpp
@@ -8,50 +8,74 @@ enum Color{
 
 class Student{
     public:
-        Student(){
-        }
         void makeMove(int Record[5][6], int Max[5][6], Color color[5][6], Color inputColor){
-            x = 5;
-            y = 6;
+            x = 1;
+            y = 1;
             for (int i = 0; i < 5; i ++){
                 for (int j = 0; j < 6; j ++){
                     if (is_playable(color, i, j, inputColor)){
-                        if (Record[i][j] == Max[i][j] - 1 && should_attack(Record, Max, color, inputColor, i, j)){
+                        //std::cout << i << " " << j << '\n';
+                        if (Record[i][j] == (Max[i][j] - 1) && should_attack(Record, Max, color, inputColor, i, j)){
                             x = i;
                             y = j;
                             return;
                         }
-                        else if (is_corner(i, j)) {
-                            if (!is_corner(x, y)) {
-                                x = i;
-                                y = j;
-                            }
-                            else if (is_corner(x, y) && Max[x][y] - Record[x][y] < Max[i][j] - Record[i][j]){
-                                x = i;
-                                y = j;
-                            }
-                        }
-                        else if (is_edge(i, j)){
-                            if (!is_corner(x, y) && !is_edge(x, y)) {
-                                x = i;
-                                y = j;
-                            }
-                            else if (is_edge(x, y) && Max[x][y] - Record[x][y] < Max[i][j] - Record[i][j]){
-                                x = i;
-                                y = j;
-                            }
-                        }
-                        else if (is_playable(color, i, j, inputColor)){
+                        else if (is_corner(i, j)){
                             if (!is_corner(x, y) && !is_edge(x, y)){
-                                if (Max[x][y] - Record[x][y] < Max[i][j] - Record[i][j]){
-                                    x = i;
-                                    y = j;
-                                }
+                                x = i;
+                                y = j;
+                            }
+                            else if (is_edge(x, y) && !is_corner(x, y)){
+                                x = i;
+                                y = j;
+                            }
+                            else if (is_corner(x, y) && Max[i][j] - Record[i][j] > Max[x][y] - Record[x][y]){
+                                x = i;
+                                y = j;
+                            }
+                        }
+                        else if (is_edge(i, j) && !is_corner(i, j)){
+                            if (!is_edge(x ,y) && !is_corner(x, y)){
+                                x = i;
+                                y = j;
+                            }
+                            else if (is_edge(x, y) && !is_corner(x, y) && Max[i][j] - Record[i][j] > Max[x][y] - Record[x][y]){
+                                x = i;
+                                y = j;
+                            }
+                        }
+                        else if (!is_edge(i, j) && !is_corner(i, j)){
+                            if (!is_edge(x, y) && !is_corner(x, y) && Max[i][j] - Record[i][j] > Max[x][y] - Record[x][y]){
+                                x = i;
+                                y = j;
                             }
                         }
                     }
                 }
             }
+        }
+        bool is_valid(const int & i, const int & j) {
+            if ((i >= 0) && (i < 5) && (j >= 0) && (j < 6)) return true;
+            else return false;
+        }
+        bool is_playable(Color color[5][6], const int & i, const int & j, Color inputColor){
+            if (color[i][j] == White || color[i][j] == inputColor) return true;
+            else return false;
+        }
+        bool is_corner (const int & i, const int & j){
+            if (i == 0 && j == 0) return true;
+            if (i == 4 && j == 0) return true;
+            if (i == 0 && j == 5) return true;
+            if (i == 4 && j == 5) return true;
+            return false;
+        }
+
+        bool is_edge (const int & i, const int & j){
+            if (i == 0) return true;
+            if (j == 0) return true;
+            if (i == 4) return true;
+            if (j == 5) return true;
+            return false;
         }
         bool should_attack(int Record[5][6], int Max[5][6], Color color[5][6], Color inputColor, const int & i, const int & j){
             if (is_valid(i - 1, j)){
@@ -75,28 +99,6 @@ class Student{
                 }
             }
             return false;
-        }
-        bool is_corner (const int & i, const int & j){
-            if (i == 0 && j == 0) return true;
-            if (i == 4 && j == 0) return true;
-            if (i == 0 && j == 5) return true;
-            if (i == 4 && j == 5) return true;
-            return false;
-        }
-        bool is_edge (const int & i, const int & j){
-            if (i == 0) return true;
-            if (j == 0) return true;
-            if (i == 4) return true;
-            if (j == 5) return true;
-            return false;
-        }
-        bool is_playable(Color color[5][6], const int & i, const int & j, Color inputColor){
-            if (color[i][j] == White || color[i][j] == inputColor) return true;
-            else return false;
-        }
-        bool is_valid(const int & i, const int & j){
-            if (i >= 0 && i < 5 && j >= 0 && j < 6) return true;
-            else return false;
         }
         int getX(){
             // Your Code
